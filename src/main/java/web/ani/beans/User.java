bfd48b96f0523ed36acd0be6df5d2ea22fc08be7
ahavni <ahavni87@gmail.com>
2015-05-09 17:37:50 +0300

    initial project structure

---
+++ src/main/java/web/ani/beans/User.java
@@ -0,0 +1,7 @@
+package web.ani.beans;
+
+/**
+ * Created by Anichka on 9.5.2015 г..
+ */
+public class User {
+}
---
+++ src/main/java/web/ani/servlets/RegistrationServlet.java
@@ -0,0 +1,7 @@
+package web.ani.servlets;
+
+/**
+ * Created by Anichka on 9.5.2015 г..
+ */
+public class RegistrationServlet {
+}
