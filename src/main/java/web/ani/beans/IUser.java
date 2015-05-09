c483f9a3d4d2f73dd31010ffc1c13e4d94684aaf
ahavni <ahavni87@gmail.com>
2015-05-09 18:49:23 +0300

    user abstract class and user interface

---
+++ src/main/java/web/ani/beans/IUser.java
@@ -0,0 +1,14 @@
+package web.ani.beans;
+
+/**
+ * Created by Anichka on 9.5.2015 г..
+ */
+public interface IUser {
+    public void listUser();
+    public void listAllUsers();
+    public void searchUser();
+    public void addUser();
+    public void removeUser();
+    public void removeAllUsers();
+    public void updateUser();
+   }
--- src/main/java/web/ani/beans/User.java
+++ src/main/java/web/ani/beans/User.java
@@ -3,5 +3,70 @@ package web.ani.beans;
 /**
  * Created by Anichka on 9.5.2015 г..
  */
-public class User {
+public abstract class User implements IUser{
+    // User attributes
+    protected String fistName;
+    protected String lastName;
+    protected int age;
+    protected String address;
+
+    // constructor
+    User(){
+        this.fistName = "";
+        this.lastName = "";
+        this.age = 0;
+        this.address = "";
+    }
+
+    User(String fistNameP, String lastNameP, int ageP, String addressP){
+        this.fistName = fistNameP;
+        this.lastName = lastNameP;
+        this.age = ageP;
+        this.address = addressP;
+    }
+
+    // getters and setter
+    public String getFistName() {
+        return fistName;
+    }
+
+    public void setFistName(String fistName) {
+        this.fistName = fistName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    public boolean equals(User userP) {
+        boolean fistNameEqual = this.fistName.equals(userP.fistName);
+        boolean lastNameEqual = this.lastName.equals(userP.lastName);
+        boolean ageEqual = (this.age == userP.age);
+        boolean addressEqual = this.address.equals(userP.address);
+
+        return
+                fistNameEqual && lastNameEqual && ageEqual && addressEqual;
+
+    }
 }
