55a64d905743e2e71e572cad56c98068f87ba946
ahavni <ahavni87@gmail.com>
2015-08-07 20:49:15 +0300

    impelemted new method for users_relations db creation

--- src/main/java/web/ani/servlets/CreateDBServlet.java
+++ src/main/java/web/ani/servlets/CreateDBServlet.java
@@ -1,5 +1,6 @@
 package web.ani.servlets;
 
+import org.apache.log4j.Logger;
 import web.ani.utils.DBUtils;
 
 import javax.servlet.ServletConfig;
@@ -13,26 +14,17 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class CreateDBServlet extends HttpServlet {
     private Connection conn;
+    final static Logger logger = Logger.getLogger(CreateDBServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Properties prop = new Properties();
-        PrintWriter out = new PrintWriter(System.out);
-        try {
-            prop = conn.getClientInfo();
-            out = resp.getWriter();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (conn != null) {
-                DBUtils.closeDBConnection(conn);
-            }
-        }
+        logger.info("Entered " + this.getClass().toString());
     }
 
     @Override
@@ -40,19 +32,14 @@ public class CreateDBServlet extends HttpServlet {
         try {
             this.conn = DBUtils.createDBConnection();
             DatabaseMetaData dbm = conn.getMetaData();
-            // check if table is there
-            ResultSet tables = dbm.getTables(null, null, "USERS", null);
-            if (tables.next()) {
-                // Table exists
-                System.out.println("Table USERS - already existing");
-            } else {
-                DBUtils.createUserTable(this.conn);
-            }
+
+            DBUtils.createUserRelationsTable(this.conn);
+
         } catch (SQLException e) {
-            System.out.println("Problem initializing the sql connection ");
+            logger.info("Problem initializing the sql connection ");
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
-            System.out.println("Some class has not been found");
+            logger.info("Some class has not been found");
             e.printStackTrace();
         }
     }
--- src/main/java/web/ani/utils/DBUtils.java
+++ src/main/java/web/ani/utils/DBUtils.java
@@ -5,22 +5,27 @@ import web.ani.beans.Customer;
 import web.ani.beans.User;
 import web.ani.servlets.SearchUserServlet;
 
+import java.awt.image.DataBufferInt;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DBUtils {
 
+    final static Logger logger = Logger.getLogger(DBUtils.class);
+
     public static Connection createDBConnection() throws SQLException, ClassNotFoundException {
         String driver = "org.apache.derby.jdbc.EmbeddedDriver";
         Class.forName(driver);
         String url = "jdbc:derby:masterDB;create=true";
+        logger.info("DB connection is initialized.");
         return DriverManager.getConnection(url);
     }
 
     public static void closeDBConnection(Connection conn) {
         try {
             conn.close();
+            logger.info("DB connection is terminated.");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -28,7 +33,7 @@ public class DBUtils {
 
     public static void createUserTable(Connection conn) throws SQLException {
         Statement stmt = null;
-        System.out.println("Creating table in given database...");
+        logger.info("Creating USERS table in given database...");
         stmt = conn.createStatement();
         try {
             String sql = "CREATE TABLE USERS " +
@@ -44,7 +49,31 @@ public class DBUtils {
                             " PRIMARY KEY (id)) ";
 
             stmt.executeUpdate(sql);
-            System.out.println("Created table in given database...");
+            logger.info("USERS table created");
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }
+    }
+
+    public static void createUserRelationsTable(Connection conn) throws SQLException {
+        Statement stmt = null;
+        logger.info("Creating USERS_RELATIONS table in given database...");
+        stmt = conn.createStatement();
+        try {
+            String sql = "CREATE TABLE USERS_RELATIONS " +
+                    "(id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), " +
+                    " consultant_id INTEGER, " +
+                    " customer_id INTEGER, " +
+                    " PRIMARY KEY (id), " +
+                    " FOREIGN KEY (consultant_id) REFERENCES USERS(id), " +
+                    " FOREIGN KEY (customer_id) REFERENCES USERS(id))";
+
+            stmt.executeUpdate(sql);
+            logger.info("USERS_RELATIONS table created");
         } catch (SQLException se) {
             //Handle errors for JDBC
             se.printStackTrace();
@@ -69,6 +98,7 @@ public class DBUtils {
                             + user.getSex() + "','"
                             + user.getUserType() + "','"
                             + user.getPassword() + "')");
+            logger.info("User with email: " + user.getEmail() + " is added to the DB");
             stmt.close();
         } catch (SQLException sqlExcept) {
             sqlExcept.printStackTrace();
@@ -98,6 +128,7 @@ public class DBUtils {
                     user.setPassword(rs.getString("pass"));
 
                     usersList.add(user);
+                    logger.info("User with email: " + user.getEmail() + " is got from DB");
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -112,7 +143,6 @@ public class DBUtils {
 
         try {
             stmt = conn.createStatement();
-//            String sql = "SELECT * FROM USERS";
             String sql = "SELECT * FROM USERS where user_type = \'" + userType + "\'";
             ResultSet rs = stmt.executeQuery(sql);
 
@@ -129,6 +159,7 @@ public class DBUtils {
                 user.setPassword(rs.getString("pass"));
 
                 usersList.add(user);
+                logger.info("User with email: " + user.getEmail() + " is got from DB");
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -154,6 +185,7 @@ public class DBUtils {
                 user.setSex(rs.getString("sex"));
                 user.setUserType(rs.getString("user_type"));
                 user.setPassword(rs.getString("pass"));
+                logger.info("User with email: " + user.getEmail() + " is got from DB");
             }
 
         } catch (SQLException e) {
@@ -179,6 +211,8 @@ public class DBUtils {
                     + "user_type='" + user.getUserType() + "',"
                     + "pass='" + user.getPassword() + "'" +
                     " WHERE EMAIL='" + user.getEmail() + "'");// get it from session?
+            logger.info("User with email: " + user.getEmail() + " has updated data");
+
             stmt.close();
         } catch (SQLException sqlExcept) {
             sqlExcept.printStackTrace();
@@ -200,6 +234,7 @@ public class DBUtils {
                     + "sex='" + user.getSex() + "',"
                     + "user_type='" + user.getUserType() + "'" +
                     " WHERE EMAIL='" + user.getEmail() + "'"); // get it from session?
+            logger.info("User with email: " + user.getEmail() + " has updated data");
             stmt.close();
         } catch (SQLException sqlExcept) {
             sqlExcept.printStackTrace();
@@ -228,6 +263,8 @@ public class DBUtils {
                 user.setSex(rs.getString("sex"));
                 user.setUserType(rs.getString("user_type"));
                 user.setPassword(rs.getString("pass"));
+
+                logger.info("User with email: " + user.getEmail() + " is found in DB");
 
                 usersList.add(user);
             }
